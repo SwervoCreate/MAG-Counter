c48220595b652f8e26eeb5d9bcb4cb96f3de2b2e
Bryce G <88700728+SwervoCreate@users.noreply.github.com>
2022-06-16 16:58:55 -0400

    Create app.js

---
+++ app.js
@@ -0,0 +1,37 @@
+
+var url = "http://store.zapier.com/api/records?Customers&secret=cd7a2785-3db3-4711-9b3f-c0bcca85ab28";
+
+var xhr = new XMLHttpRequest();
+
+function go() {
+xhr.open("GET", url);
+
+xhr.onreadystatechange = function () {
+if (xhr.readyState === 4) {
+console.log(xhr.status);
+console.log(xhr.responseText);
+
+
+data = JSON.parse(xhr.response);
+const child = document.getElementById("container").firstElementChild;
+const container = document.getElementById("container");
+const div = document.createElement("h1");
+const node = document.createTextNode(data.Customers);
+container.removeChild(child);
+div.appendChild(node);
+container.appendChild(div);
+div.className = "counter";
+div.id = "counter"
+
+
+
+}};
+
+xhr.send();
+};
+go();
+setInterval(go, 6000);
+
+
+
+
